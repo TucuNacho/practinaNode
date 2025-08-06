ee899a53c53e7ec6c1988c9af79feea3ebff0c0b
Nacho <barrojojoaquin2@gmail.com>
2025-08-06 20:05:38 -0300

    agregue mas ejemplos de colores de los textos y las parte del readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# titulo principal
+
+descripcion
+
+# Herramientas utilizadas
+
+-lista de herramientas
+-lista de herramientas
+
+# autor
--- src/8-palabraColores.js
+++ src/8-palabraColores.js
@@ -5,3 +5,4 @@ const mensaje= "Este es un mensaje con colores"
 console.log(mensaje.red);
 console.log(mensaje.rainbow);
 console.log(mensaje.bgBlue.yellow);
+console.log(mensaje.yellow);
